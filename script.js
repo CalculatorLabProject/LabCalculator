3caacd3a605dd70e807e8d216283a0d90d27c1eb
Obsaabdu <obsaabdu01@gmail.com>
2024-06-10 21:08:50 +0300

    final modification

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
-// designed by Mohammed Jafer UGR/22624/13
+// Designed by: Mohammed Jafar UGR/22624/13
+// Modified by: Obsa Abdulkadir UGR/22770/13
 var display = document.getElementById("display");
-
+answered = "";
 function inputNumber(num) {
-  if (display.value == "Error!") {
+  if (display.value == "Error!" || answered !== "") {
     display.value = num;
+    answered = "";
   } else {
     display.value += num;
   }
 }
 
-function inputDecimal() {;
-  if (display.value.indexOf(".") === -1) {
+function inputDecimal() {
+  if (answered !== "") {
+    answered = "";
+    display.value = "0.";
+  } else if (display.value === "") {
+    display.value += "0.";
+  } else if (display.value.indexOf(".") === -1) {
     display.value += ".";
   }
 }
 
+function backspace() {
+  display.value = display.value.slice(0, -1);
+  if (answered !== "") {
+    answered = "";
+  }
+}
 function clearDisplay() {
   display.value = "";
+  answered = "";
 }
 
 function inputOperator(op) {
   if (display.value !== "") {
     display.value += op;
+    if (answered !== "") {
+      answered = "";
+    }
   }
 }
 
@@ -38,3 +55,24 @@ function calculate() {
     display.value = "Error!";
   }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  function handleKeyPress(event) {
+    const key = event.key;
+    if (['1','2','3','4','5','6','7','8','9','0'].includes(key)) {
+      inputNumber(Number(key));
+    } else if (["+", "-", "*", "/"].includes(key)) {
+      inputOperator(key);
+    } else if (key === "Enter" || key === "=") {
+      calculate();
+    } else if (key === "Escape") {
+      clearDisplay();
+    } else if (key == "Backspace") {
+      backspace();
+    } else if (key == ".") {
+      inputDecimal();
+    }
+  }
+
+  document.addEventListener('keydown', handleKeyPress);
+});
